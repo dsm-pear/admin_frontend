670b28e5b20f0410104c48ea5eaccc2cd8bb7f3e
hyejun <hyejun0608@naver.com>
2020-10-28 18:37:31 +0900

    adminLogin img 경로 수정

--- src/assets/adminLogIn/index.js
+++ src/assets/adminLogIn/index.js
@@ -1,4 +1,1 @@
 export { default as LogInImg } from './logInImg.svg';
-export { default as Check } from './check.svg';
-export { default as Checked } from './checked.svg';
-export { default as Fail } from './fail.png';
