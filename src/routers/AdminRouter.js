7ec9f387c599fc5bd101a294791f1eb89dcb943c
hyejun <hyejun0608@naver.com>
2020-11-02 23:01:47 +0900

    공지사항 추가

--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -1,13 +1,14 @@
-import React, { FC, ReactElement, useEffect } from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import AdminLogin from '../components/adminLogin/AdminLogin';
+import NoticeLook from '../components/notice/NoticeLook';
 import Question from '../components/question/Question';
 const AdminRouter = () => {
-
   return (
       <Switch>
         <Route path='/admin/login' component={AdminLogin} />
         <Route path='/admin/question' component={Question} />
+        <Route path='/admin/notice' component={NoticeLook} />
       </Switch>
   );
 };
