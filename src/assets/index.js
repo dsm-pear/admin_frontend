11ca943d12d8013feecc3d128c36a41f788fd3d8
hyejun <hyejun0608@naver.com>
2020-11-10 23:33:21 +0900

    [UPDATE] img 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export { LogInImg } from './adminLogIn';
 export { BackgroundImg, BackgroundImg1 } from './background';
 export { Share } from './question';
+export { Upload, Img, Link } from './notice';
