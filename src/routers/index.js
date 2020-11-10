c51e1d199f8a872603eef10582333ef37fbab5ec
hyejun <hyejun0608@naver.com>
2020-11-10 23:28:06 +0900

    [ADD] index.js 추가

---
+++ src/routers/index.js
@@ -0,0 +1,6 @@
+export { default as AdminLogin} from '../components/adminLogin/AdminLogin';
+export { default as NoticeView } from '../components/notice/NoticeView';
+export { default as NoticeWrite } from '../components/notice/NoticeWrite';
+export { default as Question } from '../components/question/Question';
+export { default as ApproveList } from '../components/reportApprove/ApproveList';
+export { default as ViewReport } from '../components/reports/ViewReport';
