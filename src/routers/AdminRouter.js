4f56b7d8671b033c2c4ef8a8387247b9566a9b86
hyejun <hyejun0608@naver.com>
2020-11-15 23:06:19 +0900

    [UPDATE] router 추가

--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { AdminLogin, NoticeView, NoticeWrite, Question, ApproveList, ViewReport } from './index';
+import { AdminLogin, NoticeView, NoticeWrite, Question, ApproveList, ViewReport, DetailNotice, DetailApprove, DetailReport } from './index';
 
 const AdminRouter = () => {
   return (
@@ -8,9 +8,12 @@ const AdminRouter = () => {
         <Route exact path='/' component={ AdminLogin } />
         <Route path='/question' component={ Question } />
         <Route path='/notice-write' component={ NoticeWrite } />
-        <Route path='/notice' component={ NoticeView } />
-        <Route path='/approve' component={ ApproveList } />
-        <Route path='/view-report' component={ ViewReport } />
+        <Route exact path='/notice' component={ NoticeView } />
+        <Route exact path='/notice/view-notice' component={ DetailNotice } />
+        <Route exact path='/approve' component={ ApproveList } />
+        <Route path='/approve/view-approve-report' component={ DetailApprove } />
+        <Route exact path='/report' component={ ViewReport } />
+        <Route path='/report/view-report' component={ DetailReport }/>
       </Switch>
   );
 };
