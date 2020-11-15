d2f7e219d84a4547b4e4d9b6c999c7768bbfd373
hyejun <hyejun0608@naver.com>
2020-11-15 23:00:33 +0900

    [ADD] 미승인 확인 모달

---
+++ src/components/reportApprove/DetailApprove/ModalComment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as S from './style';
+
+const ModalComment = ({setIsDisapproveClick, setIsSend}) => {
+    const onClick = e => {
+        setIsDisapproveClick(false);
+        setIsSend(false);
+    }
+    return(
+        <S.ModalBackground>
+            <div>
+                <S.ModalBoxTitle>
+                    <span>PEAR</span> Admin console
+                </S.ModalBoxTitle>
+                <S.ModalBox>
+                    <div>승인 거부하시겠습니까?</div>
+                    <div>
+                        <S.Cancle onClick={onClick}>취소</S.Cancle>
+                        <S.True>확인</S.True>
+                    </div>
+                </S.ModalBox>
+            </div>
+        </S.ModalBackground>
+    )
+}
+
+export default ModalComment;
