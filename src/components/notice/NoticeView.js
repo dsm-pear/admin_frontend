ceb19df82b373cd4755bf01b72889f244af94445
hyejun <hyejun0608@naver.com>
2020-11-15 22:56:06 +0900

    [UPDATE] router link 추가

--- src/components/notice/NoticeView.js
+++ src/components/notice/NoticeView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './style';
 import Header from '../header/Header';
 import NoticeLine from './NoticeLine';
@@ -10,7 +11,7 @@ const NoticeView = () => {
             <S.LWhiteBox>
                 <div>
                     <S.Notice>공지사항</S.Notice>
-                    <S.Write>쓰기/</S.Write>
+                    <Link to='/notice-write'><S.Write>쓰기/</S.Write></Link>
                     <S.Watch>보기</S.Watch>
                 </div>
                 <div>
@@ -19,10 +20,12 @@ const NoticeView = () => {
                         <div>작성일</div>
                     </S.NoticeTitle>
                     <ul>
-                        <NoticeLine 
-                            title="보고서 작성 양식 알려드립니다."
-                            date="2020.20.20"
-                        />
+                        <Link to='/notice/view-notice'>
+                            <NoticeLine 
+                                title="보고서 작성 양식 알려드립니다."
+                                date="2020.20.20"
+                            />
+                        </Link>
                         <NoticeLine 
                             title="제출 오류 관련"
                             date="1919.19.19"
@@ -37,6 +40,17 @@ const NoticeView = () => {
                         <NoticeLine />
                         <NoticeLine />
                     </ul>
+                </div>
+                <div>
+                    <S.Turn>◀︎</S.Turn>
+                    <S.Count>
+                        <div data-id='1'>1</div>
+                        <div data-id='2'>2</div>
+                        <div data-id='3'>3</div>
+                        <div data-id='4'>4</div>
+                        <div data-id='5'>5</div>
+                    </S.Count>
+                    <S.Turn>▶︎</S.Turn>
                 </div>
             </S.LWhiteBox>
         </S.Background>
