ccb7c9ac3c1718b20ab501540f164e27521c79a8
hyejun <hyejun0608@naver.com>
2020-11-15 22:48:27 +0900

    [UPDATE] Router Link 적용

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './style'
 
 const Header = () => {
+    const [isFirstClick, setIsFirstClick] = useState(false);
+    const [isSecondClick, setIsSecondClick] = useState(false);
+    const [isThirdClick, setIsthirdClick] = useState(false);
+    const [isFourthClick, setIsFourthClick] = useState(false);
+    const onClick = e => {
+        const clickCategory = Number(e.currentTarget.dataset.id);
+        if(clickCategory === 1) {
+            setIsFirstClick(true);
+        }
+        else if(clickCategory === 2) {
+            setIsSecondClick(true);
+        }
+        else if(clickCategory === 3) {
+            setIsthirdClick(true);
+        }
+        else {
+            setIsFourthClick(true);
+        }
+    }
     return(
         <S.Flex>
             <S.Pear>
                 <span>PEAR</span> Admin console
             </S.Pear>
             <S.Size>
-                <S.Category>보고서 승인</S.Category>
-                <S.Category>보고서 보기</S.Category>
-                <S.Category>공지사항</S.Category>
-                <S.Category>문의사항</S.Category>
+                <Link to='/approve'>
+                    <S.Category data-id='1' boolean={isFirstClick} onClick={onClick}>보고서 승인</S.Category>
+                </Link>
+                <Link to='/report'>
+                    <S.Category data-id='2' boolean={isSecondClick} onClick={onClick}>보고서 보기</S.Category>
+                </Link>
+                <Link to='/notice-write'>
+                    <S.Category data-id='3' boolean={isThirdClick} onClick={onClick}>공지사항</S.Category>
+                </Link>
+                <Link to='/question'>
+                    <S.Category data-id='4' boolean={isFourthClick} onClick={onClick}>문의사항</S.Category>
+                </Link>
             </S.Size>
             <S.Circle>
                 <S.Logout>로그아웃</S.Logout>
