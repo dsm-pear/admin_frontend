ebadc2af2998d3d476216270eda4873dae7af258
hyejun <hyejun0608@naver.com>
2020-11-15 22:54:06 +0900

    [UPDATE] 이미지 경로 추가

--- src/assets/notice/index.js
+++ src/assets/notice/index.js
@@ -1,3 +1,4 @@
 export { default as Upload } from './upload.svg';
 export { default as Img } from './img.svg';
-export { default as Link } from './link.svg';
+export { default as LinkImg } from './link.svg';
+export { default as Download } from './DetailNotice/download.svg';
