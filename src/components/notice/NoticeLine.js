38ba8aab457b6257e979d6241e0008491ce1534e
hyejun <hyejun0608@naver.com>
2020-11-16 17:20:32 +0900

    [UPDATE] <></> 제거

--- src/components/notice/NoticeLine.js
+++ src/components/notice/NoticeLine.js
@@ -3,12 +3,10 @@ import * as S from './style';
 
 const NoticeLine = ({ title, date }) => {
     return (
-        <>
         <S.Line>
             <S.LineTitle>{ title }</S.LineTitle>
             <S.LineDate>{ date }</S.LineDate>
         </S.Line>
-        </>
     )
 }
 
