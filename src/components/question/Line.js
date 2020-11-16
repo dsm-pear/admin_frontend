858ebe6e9329cc8027fc5de2a12827a7b0b682a4
hyejun <hyejun0608@naver.com>
2020-11-16 22:36:59 +0900

    [UPDATE] 확인 눌렀을 때 임시로 데이터값 지움

--- src/components/question/Line.js
+++ src/components/question/Line.js
@@ -2,10 +2,24 @@ import React, {useState, } from 'react';
 import * as S from './style';
 import { Share } from '../../assets';
 
-const Line = ({ title, contents, date, email }) => {
+const Line = ({ title, contents, date, email, setData }) => {
     const [ isOpened, setIsOpened ] = useState(false);
     const handleClick = () => {
         setIsOpened(!isOpened);
+    }
+    const dummyData = [{
+        title: '승인되었다는 이메일을 받았는데 게시글에 올라가있지 않습니다.',
+        date: '2020.10.28',
+        email: 'rlagPwns@rlagPwns',
+        contents: '이메일이 잘못 온 건지 저만 안 보이는 건지 모르겠습니다.'
+    }, {
+        title: '마이페이지에 보고서가 안 보여요',
+        date: '2020.11.16',
+        email: 'rlagPwns@rlagPwns',
+        contents: '어제까지만 해도 잘 보였는데 오늘 갑자기 사라졌어요'
+    }]
+    const onclick = e => {
+        setData(dummyData)
     }
     return(
         <div>
@@ -22,7 +36,7 @@ const Line = ({ title, contents, date, email }) => {
                             <S.Line />
                             <S.Note>{ contents }</S.Note>
                         </div>
-                        <S.DeleteBtn>완료</S.DeleteBtn>
+                        <S.DeleteBtn onClick={onclick}>완료</S.DeleteBtn>
                     </S.SQBox>
                 </S.BQBox>
             }
--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 import Line from './Line';
 
 const Question = () => {
+    const dummyData = [{
+        title: '승인되었다는 이메일을 받았는데 게시글에 올라가있지 않습니다.',
+        date: '2020.10.28',
+        email: 'rlagPwns@rlagPwns',
+        contents: '이메일이 잘못 온 건지 저만 안 보이는 건지 모르겠습니다.'
+    }, {
+        title: '마이페이지에 보고서가 안 보여요',
+        date: '2020.11.16',
+        email: 'rlagPwns@rlagPwns',
+        contents: '어제까지만 해도 잘 보였는데 오늘 갑자기 사라졌어요'
+    }, {
+        title: '문의사항 드립니다',
+        date: '2020.11.16',
+        email: 'rlagPwns@rlagPwns',
+        contents: '문의사항 입니다'
+    }]
+    const [data, setData] = useState(dummyData);
     return (
         <S.Background>
             <Header />
@@ -11,42 +28,17 @@ const Question = () => {
                 <S.Title>문의사항</S.Title>
                 <S.Scontents>
                     <div>
-                        <Line 
-                            title="승인되었다는 이메일을 받았는데 게시글에 올라가있지 않습니다."
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="이메일이 잘못 온 건지 저만 안 보이는 건지 모르겠습니다."
-                        />
-                        <Line 
-                            title="마이페이지에 보고서가 안 보여요"
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="어제까지만 해도 잘 보였는데 오늘 갑자기 사라졌어요"
-                        />
-                        <Line 
-                            title="문의사항 드립니다"
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="그냥 드려봤어요"
-                        />
-                        <Line 
-                            title="문의사항 드립니다"
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="그냥 드려봤어요"
-                        />
-                        <Line 
-                            title="문의사항 드립니다"
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="그냥 드려봤어요"
-                        />
-                        <Line 
-                            title="문의사항 드립니다"
-                            date="2020.10.28"
-                            email="rlagPwns@rlagPwns"
-                            contents="그냥 드려봤어요"
-                        />
+                        {data.map(data => {
+                            return (
+                                <Line 
+                                    title={data.title}
+                                    date={data.date}
+                                    email={data.email}
+                                    contents={data.contents}
+                                    setData={setData}
+                                />
+                            )
+                        })}
                     </div>
                 </S.Scontents>
             </S.Contents>
