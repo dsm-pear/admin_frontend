6d1cadd90e85f4ab5a66a0688e4281fa20d4d6cc
hyejun <hyejun0608@naver.com>
2020-11-17 21:51:22 +0900

    [UPDATE] 더미데이터 값 생성

--- src/components/reports/DetailReport/DeleteComment.js
+++ src/components/reports/DetailReport/DeleteComment.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import * as S from './style';
 
-const DeleteComment = ({setIsClick}) => {
+const DeleteComment = ({setIsClick, setData}) => {
     const onClick = e => {
+        setIsClick(false);
+    }
+    const dummyData = [{
+        name: '김혜준',
+        text: '참신한 아이디어에 박수를 칩니다',
+        date: '2020.11.15 01:53'
+    }, {
+        name: '김혜준',
+        text: '보고서를 깔끔하게 잘 작성하셨네요!',
+        date: '2020.11.15 01:55'
+    }]
+    const onBtnClick = e => {
+        setData(dummyData);
         setIsClick(false);
     }
     return (
@@ -15,7 +28,7 @@ const DeleteComment = ({setIsClick}) => {
                     <div>댓글을 삭제하시겠습니까?</div>
                     <div>
                         <S.Cancle onClick={onClick}>취소</S.Cancle>
-                        <S.True>확인</S.True>
+                        <S.True onClick={onBtnClick}>확인</S.True>
                     </div>
                 </S.ModalBox>
             </div>
