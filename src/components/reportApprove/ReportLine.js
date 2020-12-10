caf4a0c44a74fabee437cbb21e78c5db3c73c95d
hyejun <hyejun0608@naver.com>
2020-12-10 14:41:39 +0900

    [UPDATE] props team -> writer로 바꿈

--- src/components/reportApprove/ReportLine.js
+++ src/components/reportApprove/ReportLine.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import * as S from './style';
 
-const ReportLine = ({ number, title, team, date}) => {
+const ReportLine = ({ number, title, writer, date}) => {
     return (
         <S.ReportLine>
             <S.Note>{ number }</S.Note>
             <S.Note>{ title }</S.Note>
             <S.Note>-</S.Note>
-            <S.Note>{ team }</S.Note>
+            <S.Note>{ writer }</S.Note>
             <S.Note>-</S.Note>
             <S.Note>{ date }</S.Note>
         </S.ReportLine>
