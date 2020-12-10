9c92ff5198e7bba60785e278fd9677b1e5592e89
hyejun <hyejun0608@naver.com>
2020-12-10 19:31:00 +0900

    [UPDATE] 더미데이터 삭제

--- src/components/question/Line.js
+++ src/components/question/Line.js
@@ -7,20 +7,6 @@ const Line = ({ title, contents, date, email, setData }) => {
     const handleClick = () => {
         setIsOpened(!isOpened);
     }
-    const dummyData = [{
-        title: '승인되었다는 이메일을 받았는데 게시글에 올라가있지 않습니다.',
-        date: '2020.10.28',
-        email: 'rlagPwns@rlagPwns',
-        contents: '이메일이 잘못 온 건지 저만 안 보이는 건지 모르겠습니다.'
-    }, {
-        title: '마이페이지에 보고서가 안 보여요',
-        date: '2020.11.16',
-        email: 'rlagPwns@rlagPwns',
-        contents: '어제까지만 해도 잘 보였는데 오늘 갑자기 사라졌어요'
-    }]
-    const onclick = e => {
-        setData(dummyData)
-    }
     return(
         <div>
             <S.LineBox onClick={ handleClick }>
@@ -36,7 +22,7 @@ const Line = ({ title, contents, date, email, setData }) => {
                             <S.Line />
                             <S.Note>{ contents }</S.Note>
                         </div>
-                        <S.DeleteBtn onClick={onclick}>완료</S.DeleteBtn>
+                        <S.DeleteBtn>완료</S.DeleteBtn>
                     </S.SQBox>
                 </S.BQBox>
             }
