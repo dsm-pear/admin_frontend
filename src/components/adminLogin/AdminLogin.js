d99414ee4e6ba2928906033eeaf567e73866a6f1
hyejun <hyejun0608@naver.com>
2020-12-10 19:08:18 +0900

    [UPDATE] console.log 삭제

--- src/components/adminLogin/AdminLogin.js
+++ src/components/adminLogin/AdminLogin.js
@@ -16,7 +16,6 @@ const AdminLogin = ({history}) => {
                 password
             })
             .then((res) => {
-                console.log('ok');
                 localStorage.setItem('access_token', res.data['access_token']);
                 localStorage.setItem('refresh_token', res.data['refresh_token']);
                 history.push('/report');
