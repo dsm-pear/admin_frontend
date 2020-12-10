c4be4154b7006afe3597a1b17267b4695f9442c1
hyejun <hyejun0608@naver.com>
2020-12-10 14:07:26 +0900

    [UPDATE] api 연동

--- src/components/adminLogin/AdminLogin.js
+++ src/components/adminLogin/AdminLogin.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LogInImg } from '../../assets';
-import { Link } from 'react-router-dom';
 import Input from './LogInInput';
 import * as S from './style';
+import { api } from '../../api/api';
 
-const AdminLogin = () => {
+const AdminLogin = ({history}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onLoginBtnClick = e => {
+        const loginInput = [email, password];
+        if(loginInput.indexOf('') === -1) {
+            api.post('/admin/auth', {
+                email,
+                password
+            })
+            .then((res) => {
+                console.log('ok');
+                
+                history.push('/report');
+            })
+            .catch((err) => {
+                switch(err.status) {
+                    case 400:
+                        alert('아이디 또는 비밀번호가 틀렸습니다.');
+                        break;
+                    case 403:
+                        alert('아이디가 존재하지 않습니다.');
+                        break;
+                    default:
+                        alert('로그인을 실패했습니다.');
+                }
+            })
+        } else {
+            alert('아이디와 비밀번호를 채워주세요.');
+        }
+    }
+
     return (
         <S.Background>
             <S.Whitebox>
@@ -16,16 +48,18 @@ const AdminLogin = () => {
                         <Input
                             text="아이디를 입력하시오"
                             type="text"
+                            info='email'
+                            setEmail={setEmail}
                         />
                         <Input 
                             text="비밀번호를 입력하시오"
                             type="password"
+                            info='password'
+                            setPassword={setPassword}
                         />
                     </S.Inputs>
-                    <S.LogInButton>
-                        <Link to='report'>
-                            LOGIN
-                        </Link>
+                    <S.LogInButton onClick={onLoginBtnClick}>
+                        LOGIN
                     </S.LogInButton>
                 </div>
             </S.Whitebox>
--- src/components/adminLogin/LogInInput.js
+++ src/components/adminLogin/LogInInput.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import * as S from './style';
 
-const Input = ({ text, type }) => {
+const Input = ({ text, type, info, setEmail, setPassword }) => {
+    const checkEmail = e => {
+        setEmail(e.target.value);
+    }
+    const checkPw = e => {
+        setPassword(e.target.value);
+    }
+
     return (
         <div style={{display: "flex"}} >
-            <S.Input placeholder={ text } type={ type }/>
+            <S.Input placeholder={ text } type={ type } onChange={info === 'email' ? checkEmail : checkPw}/>
         </div>
     )
 }
