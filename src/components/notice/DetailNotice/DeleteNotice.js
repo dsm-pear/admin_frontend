dea450b65274b876bde148ef1d5547aa78abf247
hyejun <hyejun0608@naver.com>
2020-12-17 15:10:59 +0900

    [UPDATE] 보고서 삭제 api 연동_1

--- src/components/notice/DetailNotice/DeleteNotice.js
+++ src/components/notice/DetailNotice/DeleteNotice.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Api } from '../../../api/api';
 import * as S from './style';
 
-const DeleteNotice = ({setIsClick}) => {
+const DeleteNotice = ({setIsClick, id}) => {
+    const history = useHistory();
+
     const onClick = e => {
         setIsClick(false);
     }
+
+    const onDeleteBtnClick = () => {
+        Api.delete(`/notice/<notice_${id}>`, {
+            headers: {
+                Authorization: localStorage.getItem('access_token')
+            }
+        })
+        .then((res) => {
+            history.push('/notice/view');
+        })
+        .catch((err) => {
+            if(err.response.status === 400) {
+                alert('공지사항 삭제를 실패했습니다.'); 
+            }
+        })
+    }
+
     return (
         <S.ModalBackground>
             <div>
@@ -15,7 +36,7 @@ const DeleteNotice = ({setIsClick}) => {
                     <div>공지사항을 삭제하시겠습니까?</div>
                     <div>
                         <S.Cancle onClick={onClick}>취소</S.Cancle>
-                        <S.True onClick={onClick}>확인</S.True>
+                        <S.True onClick={onDeleteBtnClick}>확인</S.True>
                     </div>
                 </S.ModalBox>
             </div>
