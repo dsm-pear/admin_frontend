746450e56b6df2df92e8145e6832999d7d5ced3b
hyejun <hyejun0608@naver.com>
2020-12-17 15:09:18 +0900

    [UPDATE] 보고서 작성 api 연동_1, 쓰기/보기 버튼으로 변경

--- src/components/notice/NoticeWrite.js
+++ src/components/notice/NoticeWrite.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './style';
 import Header from '../header/Header';
 import { Upload, Img, LinkImg } from '../../assets';
+import { Api } from '../../api/api';
 
 const NoticeWirte = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [file, setFile] = useState([]);
+
+    const NoticeWirte = () => {
+        Api.post('/notice', {
+            body: {
+                title,
+                description,
+                file
+            },
+            headers: {
+                Authorization: localStorage.getItem('access_token')
+            }
+        })
+        .then((res) => {
+            alert('공지사항 작성을 성공했습니다.');
+        })
+        .catch((err) => {
+            if(err.response.status === 400) {
+                alert('공지사항 작성을 실패했습니다.');
+            }
+        })
+    }
+
+    const onTitleChange = e => {
+        setTitle(e.target.value);
+    }
+    const onDescriptionChange = e => {
+        setDescription(e.target.value);
+    }
+
     return (
         <S.Background>
             <Header />
             <S.WhiteBox>
                 <div>
                     <S.Notice>공지사항</S.Notice>
-                    <S.LWrite>쓰기 /</S.LWrite>
+                    <S.LWrite>쓰기</S.LWrite>
                     <Link to='/notice/view'><S.LWatch>보기</S.LWatch></Link>
                 </div>
                 <div>
-                    <S.Title placeholder="제목을 입력해주세요."/>
+                    <S.Title placeholder="제목을 입력해주세요." onChange={onTitleChange}/>
                     <S.ContentsBox>
                         <div>
                             <S.ContentsTitle>내용입력</S.ContentsTitle>
@@ -30,9 +63,9 @@ const NoticeWirte = () => {
                                 </S.Add>
                             </div>
                         </div>
-                        <S.Contents placeholder="내용을 입력해주세요."/>
+                        <S.Contents placeholder="내용을 입력해주세요." onChange={onDescriptionChange}/>
                         <S.Addflie>파일첨부</S.Addflie>
-                        <S.Upload>
+                        <S.Upload onClick={NoticeWirte}>
                             <img src={ Upload } alt="업로드" width="12px" height="12px"/>
                             업로드
                         </S.Upload>
