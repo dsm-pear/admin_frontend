c6fdd142b0ed4b4ef8dc129e26da9128f0c1826b
hyejun <hyejun0608@naver.com>
2020-12-17 15:07:39 +0900

    [UPDATE] admin login api 연동_2

--- src/components/adminLogin/AdminLogin.js
+++ src/components/adminLogin/AdminLogin.js
@@ -2,36 +2,42 @@ import React, { useState } from 'react';
 import { LogInImg } from '../../assets';
 import Input from './LogInInput';
 import * as S from './style';
-import { api } from '../../api/api';
+import { Api } from '../../api/api';
+import { useHistory } from 'react-router-dom';
 
-const AdminLogin = ({history}) => {
+const AdminLogin = () => {
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    const onLogin = () => {
+        Api.post('/auth', {
+            email,
+            password
+        })
+        .then((res) => {
+            localStorage.setItem('access_token', res.data['access_token']);
+            localStorage.setItem('refresh_token', res.data['refresh_token']);
+            history.push('/report');
+        })
+        .catch((err) => {
+            switch(err.response.status) {
+                case 400:
+                    alert('아이디 또는 비밀번호가 틀렸습니다.');
+                    break;
+                case 403:
+                    alert('아이디가 존재하지 않습니다.');
+                    break;
+                default:
+                    alert('로그인을 실패했습니다.');
+            }
+        })
+    }
 
     const onLoginBtnClick = e => {
         const loginInput = [email, password];
         if(loginInput.indexOf('') === -1) {
-            api.post('/admin/auth', {
-                email,
-                password
-            })
-            .then((res) => {
-                localStorage.setItem('access_token', res.data['access_token']);
-                localStorage.setItem('refresh_token', res.data['refresh_token']);
-                history.push('/report');
-            })
-            .catch((err) => {
-                switch(err.status) {
-                    case 400:
-                        alert('아이디 또는 비밀번호가 틀렸습니다.');
-                        break;
-                    case 403:
-                        alert('아이디가 존재하지 않습니다.');
-                        break;
-                    default:
-                        alert('로그인을 실패했습니다.');
-                }
-            })
+            onLogin();
         } else {
             alert('아이디와 비밀번호를 채워주세요.');
         }
