3d22e53944fb4b9371fe0fc10ded3c916d543735
hyejun <hyejun0608@naver.com>
2020-12-17 15:12:05 +0900

    [UPDATE] 문의사항 삭제 api 연동_1

--- src/components/question/Line.js
+++ src/components/question/Line.js
@@ -1,17 +1,47 @@
 import React, {useState, } from 'react';
 import * as S from './style';
 import { Share } from '../../assets';
+import { Api, onRefresh } from '../../api/api';
+import { useHistory } from 'react-router-dom';
 
-const Line = ({ title, contents, date, email, setData }) => {
+const Line = ({description, date, email, id}) => {
+    const history = useHistory();
     const [ isOpened, setIsOpened ] = useState(false);
+    
     const handleClick = () => {
         setIsOpened(!isOpened);
     }
+
+    const onDeleteBtnClick = () => {
+        Api.delete(`/question/<question_${id}>`, {
+            headers: {
+                Authorization: localStorage.getItem('access_token')
+            }
+        })
+        .then((res) => {
+            history.push('/question');
+        })
+        .catch((err) => {
+            switch(err.response.status) {
+                case 400:
+                    alert('문의사항 삭제를 실패했습니다.');
+                case 403:
+                    onRefresh()
+                    .then(() => {
+                        onDeleteBtnClick()
+                    })
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
+
     return(
         <div>
             <S.LineBox onClick={ handleClick }>
                 <img src={ Share } alt="share"/>
-                <S.Qtitle>{ title }</S.Qtitle>
+                <S.Qtitle>제목</S.Qtitle>
                 <S.QDate>{ date }</S.QDate>
             </S.LineBox>
             {   isOpened &&
@@ -20,9 +50,9 @@ const Line = ({ title, contents, date, email, setData }) => {
                         <div>
                             <S.Email>{ email }</S.Email>
                             <S.Line />
-                            <S.Note>{ contents }</S.Note>
+                            <S.Note>{ description }</S.Note>
                         </div>
-                        <S.DeleteBtn>완료</S.DeleteBtn>
+                        <S.DeleteBtn onClick={onDeleteBtnClick}>완료</S.DeleteBtn>
                     </S.SQBox>
                 </S.BQBox>
             }
