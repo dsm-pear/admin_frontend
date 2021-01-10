2c86ab8dfe0dda884abefef8b487b0398d9ddecb
hyejun <hyejun0608@naver.com>
2021-01-11 02:22:36 +0900

    [UPDATE] 무한스크롤

--- src/components/question/Line.js
+++ src/components/question/Line.js
@@ -1,66 +1,63 @@
-import React, {useState, } from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import { Share } from '../../assets';
 import { Api, useRefresh } from '../../api/api';
-import { useHistory } from 'react-router-dom';
 
-const Line = ({description, date, email, id}) => {
-    const history = useHistory();
-    const [ isOpened, setIsOpened ] = useState(false);
-    
-    const handleClick = () => {
-        setIsOpened(!isOpened);
-    }
+const Line = ({ description, date, email, id, title }) => {
+  const [isOpened, setIsOpened] = useState(false);
 
-    const refreshHandler = useRefresh();
+  const handleClick = () => {
+    setIsOpened(!isOpened);
+  };
 
-    const onDeleteBtnClick = () => {
-        Api.delete(`/question/<question_${id}>`, {
-            headers: {
-                Authorization: localStorage.getItem('access_token')
-            }
-        })
-        .then((res) => {
-            history.push('/question');
-        })
-        .catch((err) => {
-            switch(err.response.status) {
-                case 400:
-                    alert('문의사항 삭제를 실패했습니다.');
-                    break;
-                case 403:
-                    refreshHandler()
-                    .then(() => {
-                        onDeleteBtnClick()
-                    })
-                    break;
-                default:
-                    break;
-            }
-        })
-    }
+  const refreshHandler = useRefresh();
 
-    return(
-        <div>
-            <S.LineBox onClick={ handleClick }>
-                <img src={ Share } alt="share"/>
-                <S.Qtitle>제목</S.Qtitle>
-                <S.QDate>{ date }</S.QDate>
-            </S.LineBox>
-            {   isOpened &&
-                <S.BQBox>
-                    <S.SQBox>
-                        <div>
-                            <S.Email>{ email }</S.Email>
-                            <S.Line />
-                            <S.Note>{ description }</S.Note>
-                        </div>
-                        <S.DeleteBtn onClick={onDeleteBtnClick}>완료</S.DeleteBtn>
-                    </S.SQBox>
-                </S.BQBox>
-            }
-        </div>
-    )
-}
+  const onDeleteBtnClick = () => {
+    Api.delete(`/question/${id}`, {
+      headers: {
+        Authorization: localStorage.getItem('access_token'),
+      },
+    })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        switch (err.response.status) {
+          case 400:
+            alert('문의사항 삭제를 실패했습니다.');
+            break;
+          case 403:
+            refreshHandler().then(() => {
+              onDeleteBtnClick();
+            });
+            break;
+          default:
+            break;
+        }
+      });
+  };
+
+  return (
+    <div>
+      <S.LineBox onClick={handleClick}>
+        <img src={Share} alt="share" />
+        <S.Qtitle>{title}</S.Qtitle>
+        <S.QDate>{date}</S.QDate>
+      </S.LineBox>
+      {isOpened && (
+        <S.BQBox>
+          <S.SQBox>
+            <div>
+              <S.Email>{email}</S.Email>
+              <S.Line />
+              <S.Note>{description}</S.Note>
+            </div>
+            <S.DeleteBtn onClick={onDeleteBtnClick}>완료</S.DeleteBtn>
+          </S.SQBox>
+        </S.BQBox>
+      )}
+    </div>
+  );
+};
 
 export default Line;
--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,71 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as S from './style';
 import Header from '../header/Header';
 import Line from './Line';
 import { Api, useRefresh } from '../../api/api';
 
 const Question = () => {
-    const [page, setPage] = useState(0);
-    const [data, setData] = useState();
-    setPage(1);
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [rdata, setRData] = useState([]);
+  const refreshHandler = useRefresh();
 
-    const refreshHandler = useRefresh();
-
-    useEffect(() => {
-        const ViewQuestion = () => {
-            Api.get(`/questions?page=${page}`, {
-                body: page,
-                headers: {
-                    Authorization: localStorage.getItem('access_token')
-                }
-            })
-            .then((res) => {
-                setData((prevData) => [...prevData, ...res.data]);
-            })
-            .catch((err) => {
-                switch(err.response.status) {
-                    case 400:
-                        alert('문의사항 불러오기를 실패했습니다.');
-                        break;
-                    case 403:
-                        refreshHandler()
-                        .then(() => {
-                            ViewQuestion()
-                        })
-                        break;
-                    default:
-                        break;
-                }
-            })
+  const ViewQuestion = (page) => {
+    Api.get('/question', {
+      body: page,
+      headers: {
+        Authorization: localStorage.getItem('access_token'),
+      },
+    })
+      .then((res) => {
+        setData(res.data);
+        setRData((prevData) => [...res.data.results, ...prevData]);
+      })
+      .catch((err) => {
+        switch (err.response.status) {
+          case 400:
+            alert('문의사항 불러오기를 실패했습니다.');
+            break;
+          case 403:
+            refreshHandler().then(() => {
+              ViewQuestion();
+            });
+            break;
+          default:
+            break;
         }
-        ViewQuestion();
-    }, [page, refreshHandler])
+      });
+  };
+  useEffect(() => {
+    console.log(page);
+    ViewQuestion(page);
+  }, [page]);
 
-    return (
-        <S.Background>
-            <Header />
-            <S.Contents>
-                <S.Title>문의사항</S.Title>
-                <S.Scontents>
-                    <div>
-                        {data &&
-                        data.results &&
-                        data.map(data => {
-                            return (
-                                <Line 
-                                    key={data.id}
-                                    id={data.id}
-                                    date={data.date}
-                                    email={data.email}
-                                    description={data.description}
-                                />
-                            )
-                        })}
-                    </div>
-                </S.Scontents>
-            </S.Contents>
-        </S.Background>
-    )
-}
+  const _infiniteScroll = () => {
+    let scrollHeight = document.querySelector('.questionBox').scrollHeight;
+    let scrollTop = document.querySelector('.questionBox').scrollTop;
+    let clientHeight = document.querySelector('.questionBox').clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', _infiniteScroll, true);
+  }, [_infiniteScroll]);
+
+  return (
+    <S.Background>
+      <Header />
+      <S.Contents>
+        <S.Title>문의사항</S.Title>
+        <S.Scontents>
+          <div className="questionBox">
+            {rdata &&
+              rdata.map((data) => {
+                return (
+                  <Line
+                    key={data.id}
+                    title={data.description}
+                    id={data.id}
+                    date={data.date}
+                    email={data.email}
+                    description={data.description}
+                  />
+                );
+              })}
+          </div>
+        </S.Scontents>
+      </S.Contents>
+    </S.Background>
+  );
+};
 
 export default Question;
