c12fe9b22c94fb5cf719aa985c9c343bd2062c16
hyejun <hyejun0608@naver.com>
2021-01-13 10:11:34 +0900

    [UPDATE] 날짜 수정

--- src/components/notice/NoticeLine.js
+++ src/components/notice/NoticeLine.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import * as S from './style';
 
-const NoticeLine = ({ title, date }) => {
-    return (
-        <S.Line>
-            <S.LineTitle>{ title }</S.LineTitle>
-            <S.LineDate>{ date }</S.LineDate>
-        </S.Line>
-    )
-}
-
+const NoticeLine = ({ title, date, id }) => {
+  const history = useHistory();
+  const dates = new Date(date);
+  const year = dates.getFullYear();
+  const month = dates.getMonth() + 1;
+  const day = dates.getDay();
+  const hours = dates.getHours();
+  const minutes = dates.getMinutes();
+  const showDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+  return (
+    <S.Line onClick={() => history.push(`/notice/view-notice/${id}`)}>
+      <S.LineTitle>{title}</S.LineTitle>
+      <S.LineDate>{showDate}</S.LineDate>
+    </S.Line>
+  );
+};
 export default NoticeLine;
