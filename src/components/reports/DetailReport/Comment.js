ba1cc7adbfb70a3452e48d08f595cf2cff9b4d90
hyejun <hyejun0608@naver.com>
2021-01-13 10:15:56 +0900

    [UPDATE] 댓글 날짜 수정

--- src/components/reports/DetailReport/Comment.js
+++ src/components/reports/DetailReport/Comment.js
@@ -3,23 +3,33 @@ import * as S from './style';
 import DeleteComment from './DeleteComment';
 import { Profile } from '../../../assets';
 
-const Comment = ({name, text, date, setData, Rid, Cid}) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const onClick = e => {
-        setIsModalOpen(true);
-    }
+const Comment = ({ name, text, date, setData, Rid, Cid }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  let commentDate = new Date(date);
+  let commentYear = commentDate.getFullYear();
+  let commentMonth = commentDate.getMonth() + 1;
+  let commentDay = commentDate.getDay();
+  let commentHours = commentDate.getHours();
+  let commentMinutes = commentDate.getMinutes();
+  let showCommentDate = `${commentYear}년 ${commentMonth}월 ${commentDay}일 ${commentHours}시 ${commentMinutes}분`;
 
-    return (
-        <S.Comment>
-            <img src={Profile} alt='프로필'/>
-            <div>{name}</div>
-            <div>{text}</div>
-            <div>{date}</div>
-            <S.DeleteBtn className='DeleteBtn' onClick={onClick}>삭제</S.DeleteBtn>
-            {isModalOpen &&
-                <DeleteComment setIsModalOpen={setIsModalOpen} setData={setData} Rid={Rid} Cid={Cid}/>
-            }
-        </S.Comment>
-    )
-}
+  const onClick = (e) => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <S.Comment>
+      <img src={Profile} alt="프로필" />
+      <div>{name}</div>
+      <div>{text}</div>
+      <div>{showCommentDate}</div>
+      <S.DeleteBtn className="DeleteBtn" onClick={onClick}>
+        삭제
+      </S.DeleteBtn>
+      {isModalOpen && (
+        <DeleteComment setIsModalOpen={setIsModalOpen} setData={setData} Rid={Rid} Cid={Cid} />
+      )}
+    </S.Comment>
+  );
+};
 export default Comment;
