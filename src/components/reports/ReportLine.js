4c7cd6893ca5cd4c2c9fc9319828cc4485380604
hyejun <hyejun0608@naver.com>
2021-01-13 10:19:57 +0900

    [UPDATE] 날짜 수정

--- src/components/reports/ReportLine.js
+++ src/components/reports/ReportLine.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import * as S from './style';
 
-const ReportLine = ({ title, name, date }) => {
-    const [isCheck, setIsCheck] = useState(false);
-    const onClick = () => {
-        setIsCheck(!isCheck);
-    }
-    return (
-        <S.Line>
-            <S.CheckBox onClick={onClick} boolean={isCheck}/>
-            <Link to='/report/view-report'>
-                <div>{ title }</div>
-                <div>{ name }</div>
-                <div>{ date }</div>
-            </Link>
-        </S.Line>
-    )
-}
+const ReportLine = ({ title, name, date, id }) => {
+  const [isCheck, setIsCheck] = useState(false);
+  const history = useHistory();
+  const dates = new Date(date);
+  const year = dates.getFullYear();
+  const month = dates.getMonth() + 1;
+  const day = dates.getDay();
+  const hours = dates.getHours();
+  const minutes = dates.getMinutes();
+  const showDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+
+  const onClick = () => {
+    setIsCheck(!isCheck);
+  };
+
+  return (
+    <S.Line onClick={() => history.push(`/report/view-report/${id}`)}>
+      <S.CheckBox onClick={onClick} boolean={isCheck} />
+      <Link to="/report/view-report">
+        <div>{title}</div>
+        <div>{name}</div>
+        <div>{showDate}</div>
+      </Link>
+    </S.Line>
+  );
+};
 
 export default ReportLine;
