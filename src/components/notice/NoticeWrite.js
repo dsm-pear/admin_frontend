0bb82a16f58ea221cc1f9668a858c623c5255eb0
hyejun <hyejun0608@naver.com>
2021-04-22 13:53:32 +0900

    [UPDATE] 파일 상태 & 파일 업로드 연동

--- src/components/notice/NoticeWrite.js
+++ src/components/notice/NoticeWrite.js
@@ -11,6 +11,7 @@ const NoticeWirte = () => {
   const [file, setFile] = useState([]);
   const refreshHandler = useRefresh();
   const history = useHistory();
+  const ACCESS_TOKEN = localStorage.getItem('access_token');
 
   const NoticeWirte = () => {
     Api.post(
@@ -21,15 +22,29 @@ const NoticeWirte = () => {
       },
       {
         headers: {
-          Authorization: localStorage.getItem('access_token'),
+          Authorization: `${ACCESS_TOKEN}`,
           'Content-Type': 'application/json',
           Accept: 'application/json',
         },
       }
     )
-      .then(() => {
-        alert('공지사항 작성을 성공했습니다.');
-        history.push('/notice/view');
+      .then((e) => {
+        const formData = new FormData();
+        formData.append('noticeFile', file);
+        const id = e.data.id;
+        FileApi.post(`/notice/files/${id}`, formData, {
+          headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+          .then(() => {
+            console.log('파일 업로드 성공');
+            history.push('/notice/view');
+          })
+          .catch(() => {
+            console.log('파일 업로드 실패');
+          });
       })
       .catch((err) => {
         switch (err.status) {
@@ -45,8 +60,14 @@ const NoticeWirte = () => {
             break;
         }
       });
+
     console.log(title);
     console.log(description);
+  };
+
+  const onFileChange = (e) => {
+    console.log(e);
+    setFile(e.target.files);
   };
 
   const onTitleChange = (e) => {
@@ -77,12 +98,25 @@ const NoticeWirte = () => {
                   <img src={LinkImg} alt="파일첨부" />
                   파일첨부
                 </S.Label>
-                <input type="file" accept=".pdf, .hwp, .txt" id="ffile" name="noticeFile" />
+                <input
+                  type="file"
+                  accept=".pdf, .hwp, .txt"
+                  id="ffile"
+                  name="noticeFile"
+                  multiple
+                  onChange={onFileChange}
+                />
                 <S.Label for="img">
                   <img src={Img} width="13px" height="13px" alt="사진첨부" />
                   사진첨부
                 </S.Label>
-                <input type="file" accept=".png, .jpeg, .jpg, .svg" id="img" />
+                <input
+                  type="file"
+                  accept=".png, .jpeg, .jpg, .svg"
+                  id="img"
+                  multiple
+                  onChange={onFileChange}
+                />
               </div>
             </div>
             <S.Contents placeholder="내용을 입력해주세요." onChange={onDescriptionChange} />
