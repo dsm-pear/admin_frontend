921602ee666c936590fc462c311803b80966dd09
hyejun <hyejun0608@naver.com>
2021-04-29 23:41:45 +0900

    [UPDATE] 날짜 오류 수정 & file 다운로드

--- src/components/notice/DetailNotice/DetailNotice.js
+++ src/components/notice/DetailNotice/DetailNotice.js
@@ -9,15 +9,18 @@ import { Api, useRefresh, FileApi } from '../../../api/api';
 const DetailNotice = () => {
   const [data, setData] = useState({ created_at: 0 });
   const refreshHandler = useRefresh();
-  const [files, setFiles] = useState({ path: '' });
+  const [file, setFiles] = useState([{ path: '' }]);
+  const [isClick, setIsClick] = useState(false);
   let { id } = useParams();
-  const date = new Date(data['created_at']);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDay();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const showDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+
+  // 공지사항 날짜
+  const noticeDate = new Date(data['created_at']);
+  const year = noticeDate.getFullYear();
+  const month = noticeDate.getMonth() + 1;
+  const date = noticeDate.getDate();
+  const hours = noticeDate.getHours();
+  const minutes = noticeDate.getMinutes();
+  const showDate = `${year}년 ${month}월 ${date}일 ${hours}시 ${minutes}분`;
   const history = useHistory();
 
   useEffect(() => {
@@ -51,10 +54,9 @@ const DetailNotice = () => {
     };
     // 파일 상태
     const GetFilesId = () => {
-      FileApi.get(`/notice/files/${id}`, {})
+      FileApi.get(`/notice/files/${id}`)
         .then((res) => {
           setFiles(res.data);
-          console.log(res.data);
         })
         .catch(() => {
           setFiles();
@@ -64,20 +66,15 @@ const DetailNotice = () => {
     ViewDetailNotice();
   }, [id]);
 
-  const [isClick, setIsClick] = useState(false);
   const DeleteBtn = (e) => {
     setIsClick(true);
   };
 
   const onDownloadBtnClick = () => {
-    FileApi.get(`/notice/${files.id}`)
-      .then(() => {
-        alert('다운로드 성공');
-      })
-      .catch((err) => {
-        alert('다운로드 실패');
-        console.log(err);
-      });
+    const ATag = document.createElement('a');
+    ATag.href = `http://54.180.224.67:3000/notice/${file[0].id}`;
+    ATag.target = '_blank';
+    ATag.click();
   };
 
   return (
@@ -98,11 +95,10 @@ const DetailNotice = () => {
         <S.Flie>
           <div>첨부파일</div>
           <S.BlackLine />
-          {files && (
+          {file && (
             <>
-              <div>{files.path}</div>
+              <div>{file[0].path}</div>
               <img src={Download} alt="다운로드" onClick={onDownloadBtnClick} />
-              <S.Preview>미리보기</S.Preview>
             </>
           )}
         </S.Flie>
@@ -114,6 +110,7 @@ const DetailNotice = () => {
             date={data.created_at}
             setIsClick={setIsClick}
             id={id}
+            file={file}
           />
         )}
         <S.Modify onClick={() => history.push(`/notice/modify/${id}`)}>수정</S.Modify>
