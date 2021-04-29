17291fa6ce3c144d7815900e039aa34552e8090b
hyejun <hyejun0608@naver.com>
2021-04-29 23:42:25 +0900

    [UPDATE] 날짜 오류 수정

--- src/components/notice/NoticeLine.js
+++ src/components/notice/NoticeLine.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import * as S from './style';
 
-const NoticeLine = ({ title, date, id }) => {
+const NoticeLine = ({ title, noticeDate, id }) => {
   const history = useHistory();
-  const dates = new Date(date);
+  // 공지사항 날짜
+  const dates = new Date(noticeDate);
   const year = dates.getFullYear();
   const month = dates.getMonth() + 1;
-  const day = dates.getDay();
+  const date = dates.getDate();
   const hours = dates.getHours();
   const minutes = dates.getMinutes();
-  const showDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+  const showDate = `${year}년 ${month}월 ${date}일 ${hours}시 ${minutes}분`;
   return (
     <S.Line onClick={() => history.push(`/notice/view-notice/${id}`)}>
       <S.LineTitle>{title}</S.LineTitle>
