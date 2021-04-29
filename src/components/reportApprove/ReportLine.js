24108d4edd1d9bd80518d5d796defb64a813a976
hyejun <hyejun0608@naver.com>
2021-04-29 23:39:26 +0900

    [UPDATE] 날짜 오류 수정

--- src/components/reportApprove/ReportLine.js
+++ src/components/reportApprove/ReportLine.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import * as S from './style';
 
-const ReportLine = ({ number, title, writer, date, id }) => {
+const ReportLine = ({ number, title, writer, dates, id }) => {
   const history = useHistory();
-  const dates = new Date(date);
-  const year = dates.getFullYear();
-  const month = dates.getMonth() + 1;
-  const day = dates.getDay();
-  const hours = dates.getHours();
-  const minutes = dates.getMinutes();
-  const showDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+  // 보고서 날짜
+  const reportDate = new Date(dates);
+  const year = reportDate.getFullYear();
+  const month = reportDate.getMonth() + 1;
+  const date = reportDate.getDate();
+  const hours = reportDate.getHours();
+  const minutes = reportDate.getMinutes();
+  const showDate = `${year}년 ${month}월 ${date}일 ${hours}시 ${minutes}분`;
   return (
     <S.ReportLine onClick={() => history.push(`/approve/view-approve-report/${id}`)}>
       <S.Note>{number}</S.Note>
