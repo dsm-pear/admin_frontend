2032faaf55b71e8e9c759d455c10f499d149e5dd
hyejun <hyejun0608@naver.com>
2021-04-30 16:33:02 +0900

    [UPDATE] prettier 적용

--- src/components/adminLogin/AdminLogin.js
+++ src/components/adminLogin/AdminLogin.js
@@ -2,69 +2,69 @@ import React, { useState } from 'react';
 import { LogInImg } from '../../assets';
 import Input from './LogInInput';
 import * as S from './style';
-import { api } from '../../api/api';
+import { Api } from '../../api/api';
+import { useHistory } from 'react-router-dom';
 
-const AdminLogin = ({history}) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const AdminLogin = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const onLoginBtnClick = e => {
-        const loginInput = [email, password];
-        if(loginInput.indexOf('') === -1) {
-            api.post('/admin/auth', {
-                email,
-                password
-            })
-            .then((res) => {
-                localStorage.setItem('access_token', res.data['access_token']);
-                localStorage.setItem('refresh_token', res.data['refresh_token']);
-                history.push('/report');
-            })
-            .catch((err) => {
-                switch(err.status) {
-                    case 400:
-                        alert('아이디 또는 비밀번호가 틀렸습니다.');
-                        break;
-                    case 403:
-                        alert('아이디가 존재하지 않습니다.');
-                        break;
-                    default:
-                        alert('로그인을 실패했습니다.');
-                }
-            })
-        } else {
-            alert('아이디와 비밀번호를 채워주세요.');
+  const onLogin = () => {
+    Api.post('/auth', {
+      email,
+      password,
+    })
+      .then((res) => {
+        localStorage.setItem('access_token', res.data.access_token);
+        localStorage.setItem('refresh_token', res.data.refresh_token);
+        history.push('/report');
+      })
+      .catch((err) => {
+        console.log(err);
+        switch (err.response.status) {
+          case 400:
+            alert('아이디 또는 비밀번호가 틀렸습니다.');
+            break;
+          case 403:
+            alert('아이디가 존재하지 않습니다.');
+            break;
+          default:
+            break;
         }
-    }
+      });
+  };
 
-    return (
-        <S.Background>
-            <S.Whitebox>
-                <div>
-                    <S.Img>
-                        <img src={ LogInImg } alt="login"/>
-                    </S.Img>
-                    <S.Inputs>
-                        <Input
-                            text="아이디를 입력하시오"
-                            type="text"
-                            info='email'
-                            setEmail={setEmail}
-                        />
-                        <Input 
-                            text="비밀번호를 입력하시오"
-                            type="password"
-                            info='password'
-                            setPassword={setPassword}
-                        />
-                    </S.Inputs>
-                    <S.LogInButton onClick={onLoginBtnClick}>
-                        LOGIN
-                    </S.LogInButton>
-                </div>
-            </S.Whitebox>
-        </S.Background>
-    )
-}
+  const onLoginBtnClick = (e) => {
+    const loginInput = [email, password];
+    if (loginInput.indexOf('') === -1) {
+      onLogin();
+    } else {
+      alert('아이디와 비밀번호를 채워주세요.');
+    }
+  };
+
+  return (
+    <S.Background>
+      <S.Whitebox>
+        <div>
+          <S.Img>
+            <img src={LogInImg} alt="login" />
+          </S.Img>
+          <S.Inputs>
+            <Input text="아이디를 입력하시오" type="text" info="email" setEmail={setEmail} />
+            <Input
+              text="비밀번호를 입력하시오"
+              type="password"
+              info="password"
+              setPassword={setPassword}
+            />
+          </S.Inputs>
+          <S.LogInButton onClick={onLoginBtnClick}>LOGIN</S.LogInButton>
+        </div>
+      </S.Whitebox>
+    </S.Background>
+  );
+};
 
 export default AdminLogin;
--- src/components/adminLogin/LogInInput.js
+++ src/components/adminLogin/LogInInput.js
@@ -2,18 +2,18 @@ import React from 'react';
 import * as S from './style';
 
 const Input = ({ text, type, info, setEmail, setPassword }) => {
-    const checkEmail = e => {
-        setEmail(e.target.value);
-    }
-    const checkPw = e => {
-        setPassword(e.target.value);
-    }
+  const checkEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const checkPw = (e) => {
+    setPassword(e.target.value);
+  };
 
-    return (
-        <div style={{display: "flex"}} >
-            <S.Input placeholder={ text } type={ type } onChange={info === 'email' ? checkEmail : checkPw}/>
-        </div>
-    )
-}
+  return (
+    <div style={{ display: 'flex' }}>
+      <S.Input placeholder={text} type={type} onChange={info === 'email' ? checkEmail : checkPw} />
+    </div>
+  );
+};
 
 export default Input;
